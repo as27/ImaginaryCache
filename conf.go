241150f2f2931b9701337df349a71a275f91b112
as27 <andreas@apxp.info>
2016-08-06 20:25:27 +0200

    Fixes #1 cache interface added

---
+++ conf.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+// ConfFile is the path to the configuration file
+var ConfFile = flag.String("conf", "conf.yaml", "Path to the conf file")
+
+// Conf stores the configuration
+var Conf Options
+
+// Options is the structure of the config file
+type Options struct {
+	ServerPort string `yaml:"ServerPort"`
+}
+
+func init() {
+	flag.Parse()
+	b, err := ioutil.ReadFile(*ConfFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = yaml.Unmarshal(b, &Conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Cache is a simple abstraction how to cache a request
+type Cache interface {
+	RequestInCache(*http.Request) bool
+	GetDataFromCache(http.ResponseWriter, *http.Request)
+	SendRequestToService(*http.Request) *http.Response
+	StoreRespond(*http.Request, io.Reader)
+}
+
+var cache Cache
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc(`/`, GetRequest).Methods("GET")
+	log.Fatal(http.ListenAndServe(Conf.ServerPort, router))
+}
+
+// GetRequest is the handler for all GET methods.
+func GetRequest(w http.ResponseWriter, r *http.Request) {
+	if cache.RequestInCache(r) {
+		cache.GetDataFromCache(w, r)
+	} else {
+		resp := cache.SendRequestToService(r)
+		var buf bytes.Buffer
+		rBody := io.TeeReader(resp.Body, &buf)
+		_, err := io.Copy(w, rBody)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		cache.StoreRespond(r, &buf)
+	}
+}
